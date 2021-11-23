a435a52a7320331c264896634448dc4d1ac34b95
atripunoska <atripunoska@gmail.com>
2021-11-23 22:55:22 +0100

    Add Hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { Email } from "react-obfuscate-email";
 
 function HeroSection() {
     return (
-        <section id="sectionOne"></section>
+        <section id="sectionOne" className="hero">
+            <h1 className="hero__title">Section One</h1>
+
+            <div className="hero__overlay">
+                <Email email="mk.contact@valtech.com" className="hero__button">
+                    <span>Contact</span>
+                </Email>
+                <p className="hero__text">Lorem ipsum dolor sit, amet consectetur</p>
+            </div>
+        </section>
     );
 }
 
